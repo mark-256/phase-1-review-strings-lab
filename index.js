e961068d7737109571a52999118f4f26d2bb5acf
Mark <markjohnguto@gmail.com>
2025-09-03 23:29:04 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+const WelcomeMessage = "Welcome to Flatbook"; 
+const myString = "template literal";
+const myNumber = 10;
+const myBoolean = false;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+"Edsger Dijkstra"[0];
+"Edsger Dijkstra".charAt(0);
+"Edsger Dijkstra".length;
+"Edsger Dijkstra"[15];
+"Edsger Dijkstra"[14];
+"Edsger Dijkstra".slice();
+"Edsger Dijkstra".slice(7);
+"Edsger Dijkstra".slice(0, 3);
+currentUser.slice(0, 1);
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
